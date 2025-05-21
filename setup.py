2017c1bfa23f1570e75250fa42f7d148df089dbc
jaisal <jaisalsharma2000@gmail.com>
2025-05-21 10:24:40 +0545

    Setup the structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Gen-AI Project',
+    version= '0.0.0',
+    author= 'J Sharma',
+    author_email= 'jaisalsharma2000@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
